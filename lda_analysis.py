fa5f9dc6a02bf1da1b242bb13dfdb4dcb6554ed6
Emma Walker <emma.s.walker@gmail.com>
2014-08-22 14:03:44 +0100

    Merge from upstream into branch

--- lda_analysis.py
+++ lda_analysis.py
@@ -2,23 +2,29 @@
 
 import logging, gensim, bz2
 import pdb
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    
+    
+def main():
+    
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    
+    # load id->word mapping (the dictionary), one of the results of step 2 above
+    id2word = gensim.corpora.Dictionary.load_from_text('dictionary_words.txt')
+    # load corpus iterator
+    mm = gensim.corpora.MmCorpus('corpus.mm')
+    # mm = gensim.corpora.MmCorpus(bz2.BZ2File('wiki_en_tfidf.mm.bz2')) # use this if you compressed the TFIDF output
+    
+    print(mm)
+    
+    #This is the pure gensim version. It uses variational Bayes
+    #lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=100, update_every=1, chunksize=5000, passes=5)
+    
+    pdb.set_trace()
+    
+    
+    lda_mallet = gensim.models.mallet('/Users/emma/mallet/bin/mallet', corpus=mm, id2word=id2word, num_topics=100, update_every=1, chunksize=5000, passes=5)
 
-# load id->word mapping (the dictionary), one of the results of step 2 above
-id2word = gensim.corpora.Dictionary.load_from_text('dictionary_words.txt')
-# load corpus iterator
-mm = gensim.corpora.MmCorpus('corpus.mm')
-# mm = gensim.corpora.MmCorpus(bz2.BZ2File('wiki_en_tfidf.mm.bz2')) # use this if you compressed the TFIDF output
-
-print(mm)
-
-lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=100, update_every=1, chunksize=5000, passes=5)
-
-pdb.set_trace()
-
-lda.print_topics(20)
-
-
+    
 if __name__ == "__main__":
 
     main()
